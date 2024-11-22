cc217033df8d34a0bd7f169895616e6d75b8386a
gupta123456 <anujg0046@gmail.com>
2024-11-22 22:14:30 +0530

    added toggle dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -26,17 +26,29 @@ import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
 const App = () => {
+  // Dark mode state
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem('darkMode') === 'true'
+  );
+
+  // Apply dark mode class to body
   useEffect(() => {
-    // check for token in LS when app first runs
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+    localStorage.setItem('darkMode', darkMode);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+
+  useEffect(() => {
     if (localStorage.token) {
-      // if there is a token set axios headers for all requests
       setAuthToken(localStorage.token);
     }
-    // try to fetch a user, if no token or invalid token we
-    // will get a 401 response from our API
     store.dispatch(loadUser());
 
-    // log user out from all tabs if they log out in one tab
     window.addEventListener('storage', () => {
       if (!localStorage.token) store.dispatch({ type: LOGOUT });
     });
@@ -45,38 +57,43 @@ const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <Navbar />
+        <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
         <Alert />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="profiles" element={<Profiles />} />
-          <Route path="profile/:id" element={<Profile />} />
-          <Route
-            path="dashboard"
-            element={<PrivateRoute component={Dashboard} />}
-          />
-          <Route
-            path="create-profile"
-            element={<PrivateRoute component={ProfileForm} />}
-          />
-          <Route
-            path="edit-profile"
-            element={<PrivateRoute component={ProfileForm} />}
-          />
-          <Route
-            path="add-experience"
-            element={<PrivateRoute component={AddExperience} />}
-          />
-          <Route
-            path="add-education"
-            element={<PrivateRoute component={AddEducation} />}
-          />
-          <Route path="posts" element={<PrivateRoute component={Posts} />} />
-          <Route path="posts/:id" element={<PrivateRoute component={Post} />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
+        <div className={darkMode ? 'app-container dark' : 'app-container light'}>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="profiles" element={<Profiles />} />
+            <Route path="profile/:id" element={<Profile />} />
+            <Route
+              path="dashboard"
+              element={<PrivateRoute component={Dashboard} />}
+            />
+            <Route
+              path="create-profile"
+              element={<PrivateRoute component={ProfileForm} />}
+            />
+            <Route
+              path="edit-profile"
+              element={<PrivateRoute component={ProfileForm} />}
+            />
+            <Route
+              path="add-experience"
+              element={<PrivateRoute component={AddExperience} />}
+            />
+            <Route
+              path="add-education"
+              element={<PrivateRoute component={AddEducation} />}
+            />
+            <Route path="posts" element={<PrivateRoute component={Posts} />} />
+            <Route
+              path="posts/:id"
+              element={<PrivateRoute component={Post} />}
+            />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </div>
       </Router>
     </Provider>
   );
