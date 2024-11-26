3001dddb10a4b3e0359abd9689212c6cd8a2bdf2
gupta123456 <anujg0046@gmail.com>
2024-11-26 19:06:27 +0530

    changes made

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const corsOptions = {
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
   credentials: true,
 };
 
